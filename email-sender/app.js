5bb808dacd7f94b8d1a9a49d650aeadf1335d75e
ilovski5 <ilovski5@gmail.com>
2022-05-28 20:02:14 +0200

    bodyParser middleware & POST method route

--- email-sender/app.js
+++ email-sender/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -13,8 +14,17 @@ app.set('views', './views');
 // Static folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.post('/send', (req, res) => {
+    const { name, email, subject, message } = req.body;
+    res.status(200).send('Email has been sent.');
 });
 
 app.listen(port, () => { console.log(`Listening on port ${port}...`)});
