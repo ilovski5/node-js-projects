58d224b717428b20f9b4b56c36525a7c47cebb03
ilovski5 <ilovski5@gmail.com>
2022-05-28 21:01:32 +0200

    nodemailer setup

--- email-sender/app.js
+++ email-sender/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const port = 3000;
@@ -22,9 +23,36 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.post('/send', (req, res) => {
+app.post('/send', async (req, res) => {
     const { name, email, subject, message } = req.body;
-    res.status(200).send('Email has been sent.');
+    const emailMessage = {
+        from: `"${name}" <${email}>`,
+        to: 'bar@example.com, baz@example.com',
+        subject: subject,
+        text: message,
+    };
+
+    try {
+        const testAccount = await nodemailer.createTestAccount();
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false,
+            auth: {
+                user: testAccount.user,
+                pass: testAccount.pass,
+            },
+        });
+
+        const info = await transporter.sendMail(emailMessage);
+
+        console.log('Message sent:', info.messageId);
+        console.log('Preview URL:', nodemailer.getTestMessageUrl(info));
+        res.status(200).send('Message sent successfully!');
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).send(`Error: ${error.message}`);
+    }
 });
 
 app.listen(port, () => { console.log(`Listening on port ${port}...`)});
