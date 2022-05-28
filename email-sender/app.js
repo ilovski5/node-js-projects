74102ab6a9e0c05bd611d0fe9a14edd7ddfa294a
ilovski5 <ilovski5@gmail.com>
2022-05-28 19:53:36 +0200

    contact form html and css

--- email-sender/app.js
+++ email-sender/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+// View engine setup
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home');
