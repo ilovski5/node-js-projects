1c2a78afb6d29c0fd39a62264d5027fcda1b4159
ilovski5 <ilovski5@gmail.com>
2022-05-31 20:59:19 +0200

    basic express server setup

--- chat-app/app.js
+++ chat-app/app.js
@@ -1,1 +1,16 @@
-//
+const express = require('express');
+const { engine } = require('express-handlebars');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+// View engine setup
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () => { console.log(`Listening on port ${port}...`)});
