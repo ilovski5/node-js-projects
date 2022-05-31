b348fc25c42847a9062d59b504121580aaad1534
ilovski5 <ilovski5@gmail.com>
2022-05-31 21:28:38 +0200

    home and chat page html/css

--- chat-app/app.js
+++ chat-app/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -9,8 +11,20 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Static directory
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.listen(port, () => { console.log(`Listening on port ${port}...`)});
+app.post('/chat', (req, res) => {
+    const { username } = req.body;
+    res.render('chat', { username });
+});
+
+app.listen(port, () => { console.log(`Listening on port ${port}...`) });
