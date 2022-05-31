2267d89b468c8b2e4b7c78a10791a00804337bdb
ilovski5 <ilovski5@gmail.com>
2022-05-31 21:37:16 +0200

    integrating socket.io

--- chat-app/app.js
+++ chat-app/app.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 const port = process.env.PORT || 3000;
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 // View engine setup
 app.engine('handlebars', engine());
@@ -27,4 +31,12 @@ app.post('/chat', (req, res) => {
     res.render('chat', { username });
 });
 
-app.listen(port, () => { console.log(`Listening on port ${port}...`) });
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    });
+});
+
+server.listen(port, () => { console.log(`Listening on port ${port}...`) });
