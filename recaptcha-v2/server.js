876f494a92d9d8624796cafa9dfdc33b5f51719f
ilovski5 <ilovski5@gmail.com>
2022-06-05 17:29:00 +0200

    recaptcha server side implementation

--- recaptcha-v2/server.js
+++ recaptcha-v2/server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
+const bodyParser = require('body-parser');
 require('dotenv').config();
+require('isomorphic-fetch');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const { SITE_KEY, SECRET_KEY } = process.env;
-const comments = ['comment 1', 'comment 2'];
+const comments = [];
 
 // View engine setup
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Static directory
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -22,6 +28,26 @@ app.get('/', (req, res) => {
 
 app.get('/comments', (req, res) => {
   res.json(comments.reverse());
+});
+
+app.post('/add', async (req, res) => {
+  const recaptchaResponse = req.body['g-recaptcha-response'];
+  const comment = req.body['comment'];
+
+  if (!recaptchaResponse || !comment) {
+    return res.status(400).end();
+  }
+
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${SECRET_KEY}&response=${recaptchaResponse}`;
+  const options = { method: 'POST' };
+
+  const result = await fetch(url, options);
+  const { success } = await result.json();
+
+  if (!success) return res.status(400).end();
+
+  comments.push(comment);
+  return res.status(200).end();
 });
 
 app.listen(port, () => {
