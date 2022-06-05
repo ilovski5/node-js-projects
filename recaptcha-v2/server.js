63a4255a7022e945cf62e12601686976211e2680
ilovski5 <ilovski5@gmail.com>
2022-06-05 17:01:55 +0200

    html and css

--- recaptcha-v2/server.js
+++ recaptcha-v2/server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const { SITE_KEY, SECRET_KEY } = process.env;
 
 // View engine setup
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+// Static directory
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home', { SITE_KEY });
 });
 
 app.listen(port, () => {
